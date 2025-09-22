8d359ca610aebc332ccb2684dcfc5d905bd25bcf
Anurag Khanna <99660777+annuraagggIIIT@users.noreply.github.com>
2025-09-22 23:14:38 +0530

    Time: 40 ms (71.79%) | Memory: 46.8 MB (38.27%) - LeetSync

--- 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
+++ 560-subarray-sum-equals-k/subarray-sum-equals-k.cpp
@@ -1,31 +1,31 @@
-#include<bits/stdc++.h>
-using namespace std ;
 class Solution {
 public:
     int subarraySum(vector<int>& arr, int k) {
-       int count =0 ; 
-       int n =arr.size();
-       vector<int> v(n, 0);
-       v[0]=arr[0];
-       for(int i =1; i<n ;i++){
-        v[i]=v[i-1] +arr[i];
-       }
-       unordered_map<int,int>m;
-       for(int j=0; j<n;j++)
-       {
-        if(v[j]==k){
-            count ++;
+         int n =arr.size();
+        int count = 0 ; 
+        vector<int> p(n,0);
+        p[0]=arr[0];
+        for(int i=1 ; i<n;i++ ){
+            p[i]=p[i-1]+arr[i];
+        }
+
+        unordered_map<int,int>m;
+        for(int j=0; j<n;j++){
+            if(p[j]==k){
+                count++;
+            }
+
+            int val=p[j]-k;
+            if(m.find(val)!=m.end()){
+                count+=m[val];
+            }
+            
+            if(m.find(p[j])==m.end()){
+                m[p[j]]=0;
+            }
+            m[p[j]]++;
 
         }
-        int val = v[j] - k ; 
-        if(m.find(val)!=m.end()){
-            count+=m[val];
-        }
-        if(m.find(v[j])==m.end()){
-            m[v[j]]=0;
-        }
-        m[v[j]]++;
-       }
-       return count;
+        return count;
     }
 };
